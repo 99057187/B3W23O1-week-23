698140a4f0634e677bdfa90f2ad9ceafe7d0e202
anthony <99057187@mydavinci.nl>
2020-03-24 14:52:53 +0100

    no message

--- lab 2/pages/onderwerp1.php
+++ lab 2/pages/onderwerp1.php
@@ -6,8 +6,16 @@
 </head>
 <body>
 	<div >
-		<h1 id=kopje1>games</h1>
-		<p id="tekst1">hello</p>
+		<h1 id=kopje1>minecraft</h1>
+		<p id="tekst1">In dit spel kan men in een uitgestrekt gebied kubusvormige objecten plaatsen op een rooster. Het spel genereert automatisch een omgeving met grondstoffen, tegenstanders, dieren, grot- en tunnelsystemen, bergformaties en meren. De speler heeft een eigen avatar, waarmee hij de omgeving actief kan aanpassen.
+
+Het spel kan op een server, met een LAN-verbinding of offline worden gespeeld; in het laatste geval door het (tegen betaling) te downloaden van de officiële website van Minecraft. De betaalde versie op de pc is inmiddels[wanneer?] al meer dan 24 miljoen keer verkocht en is intussen over alle platformen meer dan 110 miljoen keer verkocht.[1]
+
+Minecraft is geschreven in Java en het werd aanvankelijk ontwikkeld door Markus Persson en tegenwoordig door het door hem opgerichte bedrijf, Mojang AB. De officiële versie is in Nederland uitgekomen op 18 november 2011. Het werd gepresenteerd tijdens een conventie voor Minecraft-spelers, genaamd MineCon 2011.</p>
+
+<h1>"Survival"</h1>
+Een van de manieren om Minecraft te spelen is via de modus "Survival", wat inhoudt dat de speler in een bepaalde wereld moet zien te overleven. Om in leven te blijven moet de speler op zoek gaan naar voedsel, dat verkregen kan worden door bijvoorbeeld dieren te slachten of graan en groente te verbouwen. Ook moet de speler zich verdedigen tegen vijandelijke mobs die 's nachts ontstaan en de speler proberen te doden. In de survivalmodus zal de speler zelf bouwmaterialen moeten verzamelen door het omhakken van bomen en het uithakken van gesteenten.
+<img src="images/minecraft.jfjf">
 	</div>
 </body>
 </html>
