72d269e27f1a8c97087217a94ba670dd190f18dd
anthony <99057187@mydavinci.nl>
2020-03-27 11:30:29 +0100

    no message

---
+++ B3W4O1 dynamische applicatie/vormgeving/index.php
@@ -0,0 +1,45 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="mysql";
+$dbname="dynamische applicatie";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}
+catch (PDOException $e){
+    echo "connection failed" . $e->getMessage();
+}
+$query = $conn->prepare('SELECT * FROM characters ORDER BY name');
+$query->execute();
+$result = $query->fetchall();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>All Characters</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="resources/css/style.css" rel="stylesheet"/>
+</head>
+<body>
+<header><h1>Alle [X] characters uit de database</h1>
+
+</header>
+<div id="container">
+    <?php
+        foreach ($result as $name){
+    ?> 
+            <img src="resources/images/<?php echo $name["avatar"]?>" id="image" style="width: 100px; height: 100px; border-radius: 50%">
+            <?php
+                echo "<a href='character.php'>" . $name['name']."</a>"." &emsp;"."<i class=\"fas fa-heart\"></i>".$name['health'] ." &emsp;"."<i class=\"fas fa-fist-raised\"></i>" . $name['attack'] . " &emsp;"."<i class=\"fas fa-shield-alt\"></i>" . $name['defense'] . "<br>" . "<br>";
+            }?>  
+        
+    
+    <a class="item" href="character.php"> 
+    </a>
+</div>
+<footer>&copy; [jenaam] 2020</footer>
+</body>
+</html>
