fa09669bed1b5b70244ba966ad66193eaa8597a7
anthony <99057187@mydavinci.nl>
2020-03-27 15:19:11 +0100

    no message

--- B3W4O1 dynamische applicatie/vormgeving/index.php
+++ B3W4O1 dynamische applicatie/vormgeving/index.php
@@ -14,6 +14,9 @@ catch (PDOException $e){
 $query = $conn->prepare('SELECT * FROM characters ORDER BY name');
 $query->execute();
 $result = $query->fetchall();
+$statement = $conn->prepare('SELECT COUNT(*) FROM characters');
+$statement->execute();
+$count = $statement->fetchAll();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,22 +27,21 @@ $result = $query->fetchall();
     <link href="resources/css/style.css" rel="stylesheet"/>
 </head>
 <body>
-<header><h1>Alle [X] characters uit de database</h1>
+<header><h1>All <?= $count[0][0] ?> characters uit de database</h1>
 
 </header>
 <div id="container">
     <?php
         foreach ($result as $name){
-    ?> 
+    ?>      
             <img src="resources/images/<?php echo $name["avatar"]?>" id="image" style="width: 100px; height: 100px; border-radius: 50%">
-            <?php
-                echo "<a href='character.php'>" . $name['name']."</a>"." &emsp;"."<i class=\"fas fa-heart\"></i>".$name['health'] ." &emsp;"."<i class=\"fas fa-fist-raised\"></i>" . $name['attack'] . " &emsp;"."<i class=\"fas fa-shield-alt\"></i>" . $name['defense'] . "<br>" . "<br>";
-            }?>  
-        
-    
+                <?php
+                    echo "<a href='character.php?id={$name['id']}' href='character.php'>" . $name['name'] . "</a>" . " &emsp;" . "<i class=\"fas fa-heart\"></i>" . $name['health'] ." &emsp;" . "<i class=\"fas fa-fist-raised\" ></i>" . $name['attack'] . " &emsp;" . "<i class=\"fas fa-shield-alt\"></i>" . $name['defense'] . "<br>" . "<br>";
+                    }
+                    ?> 
     <a class="item" href="character.php"> 
     </a>
 </div>
-<footer>&copy; [jenaam] 2020</footer>
+<footer>&copy; [Anthony Inocencio Ramos] 2020</footer>
 </body>
 </html>
